71b100556e265409447d59ac6bebe1599aee47c3
woonees2 <hello_world_@kakao.com>
2022-07-25 13:40:30 +0900

    STYLE : 글로벌 스타일 설정 및 프로젝트 구조 설정

---
+++ roommate-project/src/design/colorPalette.ts
@@ -0,0 +1,7 @@
+export const mainRed: string = "#FF4949";
+export const mainOrange: string = "#FF8D29";
+export const mainYellow: string = "#FFCD38";
+export const mainPurple: string = "#541690";
+export const backgroundGrey: string = "#f2f2f2";
+export const mainWhite: string = "#FFFDFD";
+export const mainBlack: string = "#333333";
---
+++ roommate-project/src/design/globalStyles.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+import { mainWhite } from "./colorPalette";
+
+export default createGlobalStyle`
+max-width: 76.8rem;
+background: ${mainWhite};
+margin: 0 auto;
+`;
